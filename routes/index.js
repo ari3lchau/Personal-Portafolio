36b88cf51613eda82549635cdf24be0c6a2f7034
Ariel Chau <arielchau56z@gmail.com>
2022-09-28 19:35:35 -0400

    pushed nav bar to heroku

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,20 @@ router.get('/about',function(req,res,next){
   res.render('about',{ title: 'Express in about page' });
 })
 
+router.get('/aboutme',function(req,res,next){
+  res.render('aboutme',{ title: 'Express in about Me page' });
+})
+
+router.get('/contact',function(req,res,next){
+  res.render('contact',{ title: 'Express in Contact page' });
+})
+
+router.get('/projectpage',function(req,res,next){
+  res.render('projectpage',{ title: 'Express in Project page' });
+})
+
+router.get('/service',function(req,res,next){
+  res.render('service',{ title: 'Express in service page' });
+})
+
 module.exports = router;
