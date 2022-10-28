03b0aeed379c58a44ad248657cc6d38cb9d56ee1
Ariel Chau <arielchau56z@gmail.com>
2022-10-28 18:13:26 -0400

    update URI and work with Mongodb Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI":"mongodb://localhost/individual_list"
+    //"URI":"mongodb://localhost/individual_list"
+    "URI" : "mongodb+srv://ariel:Bu6C0uj9C1Pzt83n@cluster0.om0r1yv.mongodb.net/individual_list?retryWrites=true&w=majority"
 }
